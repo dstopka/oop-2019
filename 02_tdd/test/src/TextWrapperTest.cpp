b0d4468d05469d8b20a9cbe1316409679d08c5b9
dstopka <dariusz.marek.stopka@gmail.com>
2019-10-14 16:33:41 +0200

    [red] column getter

--- 02_tdd/test/src/TextWrapperTest.cpp
+++ 02_tdd/test/src/TextWrapperTest.cpp
@@ -7,3 +7,8 @@ class TextWrapper{
 TEST(TextWrapper, CreateInstance) {
     auto wrapper = TextWrapper{};
 }
+
+TEST(TextWrapper, HasColumsGetter){
+    auto wrapper = TextWrapper{};
+    EXPECT_EQ(10, wrapper.columns());
+}
