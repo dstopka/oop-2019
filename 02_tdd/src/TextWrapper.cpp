6ab4b3507f3aa20bcddb80b716f6d3b1a53828e9
dstopka <dariusz.marek.stopka@gmail.com>
2019-10-17 17:05:49 +0200

    [green] text wrapper (4c, "ab cde")

--- 02_tdd/src/TextWrapper.cpp
+++ 02_tdd/src/TextWrapper.cpp
@@ -27,6 +27,15 @@ std::string TextWrapper::wrapText()
             }
             else
             {
+                if(text.find_last_of(" ", i) != std::string::npos)
+                {
+                    std::string str = text.substr(text.find_last_of(" ", i), columnsNo + 1);
+                    if(str.back() == ' ' || str.length() <= columnsNo + 1)
+                    {
+                        text.insert(text.find_last_of(" ", i)+1, "\n");
+                    }
+                }
+                else
                     text.insert(i++, "\n");
             }
         }
